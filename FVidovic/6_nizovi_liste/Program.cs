e858ce239c2310bde314eb95eefa733e0efbf49c
office10 <office10@ucione.local>
2020-02-10 21:38:20 +0100

    Nakon pullanja sa algebre

---
+++ FVidovic/6_nizovi_liste/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _6_nizovi_liste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int[] niz = new int[3];
+            niz[0] = 55;
+            niz[1] = 66;
+            niz[2] = 77;
+             // niz[3] = 88; ovaj ima samo 3 elementa pa ovaj necemo
+
+            for ( int i = 0; i < niz.Length; i++)
+            {
+                Console.WriteLine(niz[i]);
+            }
+            
+            foreach (int item in niz)
+            {
+                Console.Write(" " + item);
+            }
+
+
+
+
+        }
+    }
+}
---
+++ FVidovic/zavrsni_uspjeh/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+namespace zavrsni_uspjeh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesi broj predmeta");
+            // todo unesi broj predmeta
+            // za svaki predmet unesi ocjenu
+            // za sve poz racuna prosjek
+            // inace ispisuje uspjeh nedovoljan
+            // za pogresno ispisi gresku i ponovi unos
+            int broj_pred = int.Parse(Console.ReadLine());
+            int suma = 0;
+            int ulaz;
+            for (int i = 0; i < broj_pred; i++)
+            {
+                Console.WriteLine("Unesi {0}. ocijenu:",(i+1));
+                try
+                {
+                    ulaz = int.Parse(Console.ReadLine());                                     
+    }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("neispravan unos!\n", ex.Message);
+                    i--;
+                    continue;
+                }
+                if (ulaz < 2)
+                {
+                    suma = 0;
+                    break;
+                }
+                else
+                    suma += ulaz;
+            }
+            if (suma == 0)
+            {
+                Console.WriteLine("Uspjeh je nedovoljan");
+            }
+            else
+            {
+                Console.WriteLine("Prosjek ocjena je {0:F2}",((float)suma / (float)broj_pred));
+
+            }
+
+
+
+
+
+
+
+
+
+        }
+
+
+    }
+}
+
+
