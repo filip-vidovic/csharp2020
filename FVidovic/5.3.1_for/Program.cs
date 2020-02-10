ddba42527b28539be958882d44df2a081c924167
office10 <office10@ucione.local>
2020-02-10 21:30:32 +0100

    Nizovi

--- FVidovic/5.3.1_for/Program.cs
+++ FVidovic/5.3.1_for/Program.cs
@@ -8,11 +8,58 @@ namespace _5._3._1_for
         {
             Console.WriteLine("Petlja for");
 
-             for (int i = 0; i < 10; i++)
+            Console.WriteLine("Brojevi od 0 do 9");
+
+            for (int i = 0; i < 10; i++)
               {
-                Console.WriteLine(i+" ");
+                Console.Write(i+" ");
               }
-           
+
+            Console.WriteLine("\nNeparni veći od 1 a manji ili jednaki 20");
+
+            for (int i = 3; i <= 20; i += 2)
+            {
+                Console.Write(i + " ");
+            }
+
+
+            Console.WriteLine("\nBrojevi od 1 do 100 djeljivi sa 5");
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 5 == 0 )
+                Console.Write(i + " ");
+            }
+
+            Console.WriteLine("\nSuma prvih 100 prirodnih brojeva");
+
+            int zbroj = 0;
+            for (int i = 1; i <= 100; i++)
+            {
+                zbroj += i;  
+            }
+            Console.Write( zbroj );
+
+            Console.WriteLine("\nFaktorijel X");
+
+
+            int faktorijel = 1;
+            int broj = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= broj; i++)
+            {
+                faktorijel *= i;
+                
+            }
+            Console.Write(faktorijel);
+
+            Console.WriteLine("\nxxxxxxxx");
+
+            for (int i = x; i <= x; i += x)
+            {
+                Console.Write(i + " ");
+            }
+
         }
+
     }
 }
