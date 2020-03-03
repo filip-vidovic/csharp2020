361f5387262ee208e373aa2c4e3b5e8c24b02db7
office10 <office10@ucione.local>
2020-03-03 21:58:47 +0100

    Pripreme za ispit

---
+++ FVidovic/Svakadvadrugaslova/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Svakadvadrugaslova
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //U testu. napraviti program koji ispisuje svaka dva slova pa dva slova izbaci, također izbaciti razmake
+            Console.WriteLine("Upišite text");
+            string txt = Console.ReadLine();
+            txt=txt.Replace(" ","");
+            Console.WriteLine(txt);
+            Console.WriteLine(txt.Substring(0, 2));
+
+
+        }
+    }
+}
---
+++ FVidovic/rastavitistringnaliste/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+
+namespace rastavitistringnaliste
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //napraviti 3 liste, u jednu spremiti svaku rijeć koja pocinje na a, u drugu sprmeiti 
+            //svaku koja pocinje na i a u trecu sve ostale
+
+
+            //Ne radi, zašto?,0
+
+            string txt = Console.ReadLine();
+            Console.WriteLine(txt);
+            ArrayList a = new ArrayList();
+            ArrayList i = new ArrayList();
+            ArrayList o = new ArrayList();
+            string[] rijec = txt.Split(" ");
+
+            for (int u = 0; u < rijec.Length; u++)
+            {
+
+                if (rijec[u].Substring(0, 1) == ("a") || rijec[0].Substring(0, 0) == ("A"))
+                {
+                    a.Add(rijec[u]);
+                }
+                else if (rijec[u].Substring(0, 1) == ("i") || rijec[0].Substring(0, 0) == ("I"))
+                {
+                    i.Add(rijec[u]);
+                }
+                else
+                {
+                    o.Add(rijec[u]);
+                }
+            }
+
+            Console.WriteLine("Rijeći na a: " + a.Count + " , rijeći na i: " + i.Count + " , a ostalih rijeći je: " + o.Count);
+
+        }
+    }
+}
